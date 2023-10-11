a8b38c8fea4c8c03a3fa00caa0d238eb90fd9ad1
Dulmini Guruge <agg362@usask.ca>
2023-10-11 15:02:33 -0600

    2023_10_11

--- Interface_E1.py
+++ Interface_E1.py
@@ -3,8 +3,10 @@ Interface for Daily Eamil list
 """
 
 import sys
+from PyQt5 import QtWidgets
+
 from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication,QLabel,
-                             QHBoxLayout, QVBoxLayout, QGridLayout,QTextEdit,QLineEdit)
+                             QHBoxLayout, QVBoxLayout, QGridLayout,QTextEdit,QLineEdit,QTimeEdit,QCheckBox)
  
 class PyQtLayout(QWidget):
  
@@ -43,18 +45,101 @@ class PyQtLayout(QWidget):
         btn_email.resize(60,40)
         btn_email.move(40,40)
 
+
         hbox_receipients= QHBoxLayout()
         hbox_receipients.addLayout(vbox_recepient)
         hbox_receipients.addWidget(txt_recepient)
         hbox_receipients.addWidget(btn_email)
 
+        #add the timer
+        lbl_timer = QLabel() 
+        lbl_timer.setText("Schedule timer")
+        lbl_timer.adjustSize()
+    
+        #edit time
+        timer = QTimeEdit()
+
+        #hbox for the timer
+        hbox_timer= QHBoxLayout()
+        hbox_timer.addWidget(lbl_timer)
+        hbox_timer.addWidget(timer)
+
+        #content
+        lbl_timer = QLabel() 
+        lbl_timer.setText("Select Content")
+        lbl_timer.adjustSize()
+
+        check_1 = QCheckBox()
+        check_1.setText("Option1")
+
+        check_2 = QCheckBox()
+        check_2.setText("Option2")
+
+        check_3 = QCheckBox()
+        check_3.setText("Option3")
+
+        check_4 = QCheckBox()
+        check_4.setText("Option4")
+
+
+        #vbox to add text and the button
+        vbox_checkbox1 = QVBoxLayout()
+        vbox_checkbox1.addWidget(check_1)
+        vbox_checkbox1.addWidget(check_2)
+
+        #vbox to add text and the button
+        vbox_checkbox2 = QVBoxLayout()
+        vbox_checkbox2.addWidget(check_3)
+        vbox_checkbox2.addWidget(check_4)
+
+        #hbox for the timer
+        hbox_checkbox= QHBoxLayout()
+        hbox_checkbox.addWidget(lbl_timer)
+        hbox_checkbox.addLayout(vbox_checkbox1)
+        hbox_checkbox.addLayout(vbox_checkbox2)
+
+
+        #sender
+        lbl_sender = QLabel() 
+        lbl_sender.setText("Sender")
+        lbl_sender.adjustSize()
+
+        #username
+        txt_senderemail = QLineEdit()
+
+        #password
+        txt_password = QLineEdit()
+        txt_password.setEchoMode(QLineEdit.Password)        
+       
+
+        #sender
+        hbox_sender= QHBoxLayout()
+        hbox_sender.addWidget(lbl_sender)
+        hbox_sender.addWidget(txt_senderemail)
+        hbox_sender.addWidget(txt_password)
+
+        #buttons to update details
+        btn_settings = QPushButton('Update settings')
+    
+        btn_manual = QPushButton('Send Manually')
+
+        hbox_update_details= QHBoxLayout()
+        hbox_update_details.addWidget(btn_settings)
+        hbox_update_details.addWidget(btn_manual)
+
+    
+
         vbox_main = QVBoxLayout()
         vbox_main.addWidget(lbl_name)
-        vbox_main.addLayout(hbox_receipients)   
+        vbox_main.addLayout(hbox_receipients)
+        vbox_main.addLayout(hbox_timer)
+        vbox_main.addLayout(hbox_checkbox)
+        vbox_main.addLayout(hbox_sender)
+        vbox_main.addLayout(hbox_update_details)   
         
         self.setLayout(vbox_main)
         self.setGeometry(600, 600, 600, 600)
-        self.setWindowTitle('PyQt5 Layout')
+        self.setWindowTitle('Daily Emails')
         self.show()
 
 
